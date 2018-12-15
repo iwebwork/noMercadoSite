84cf458616d0c8430322a3ed51432f84c3f8f129
iwebwork <iwebwork@gmail.com>
2018-12-15 01:07:26 -0200

    Base do menu feito

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<link rel="shortcut icon" href="./Imagens/Originais/ldpi.png" >
+<head lang="pt-br">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset='utf-8' http-equiv="Content-Language" content="pt-br"/>
+<title style="font-family: Canaro;">No Mercado</title>
+<!--  CSS pessoais -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/_nav.css">
+
+<!--  JS pessoais -->
+<script type="text/javascript" src="js/_nav.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+
+</head>
+<body class="body">
+	<!--
+	<div class="container-fluid NoMercadoEstilo area-menu">
+		<nav class="">
+			<div class="row">
+				<div class="col-sm-4 area-logo d-flex justify-content-center">
+					<a href="index.php" class="">
+						<img class="img-fluid logo" alt="Responsive image" src="Imagens/originais/branco.png">
+					</a>
+				</div>
+				<div class="col-sm-8 d-flex justify-content-start" id="areaMenu">
+					
+					    <div class="nav-item">
+					      <a class="nav-link link" href="index.php">Home</a>
+					    </div>
+					    <div class="nav-item">
+					      <a class="nav-link link" href="#">Lojas</a>
+					    </div>
+					    <div class="nav-item">
+					      <a class="nav-link link" href="#">Sobre</a>
+					    </div>
+					    <div class="nav-item">
+					      <a class="nav-link link" href="#">Contato</a>
+					    </div>
+						<button class="navbar-toggler">
+						    <span class="navbar-toggler-icon"> <img src="Imagens/menu.png"></span>
+						</button>
+				</div>
+				
+			</div>
+		</nav>
+	</div>-->
+
+	<div class="container-fluid NoMercadoEstiloBranco area-menu">
+		<nav class="navbar navbar-expand-md navbar-dark">
+		  	<div class="col-sm-4 area-logo d-flex justify-content-center">
+				<a href="index.php" class="">
+					<img class="img-fluid logo" alt="Responsive image" src="Imagens/Originais/Vermelho.png">
+				</a>
+				<!--<span onclick="displayMenu()" class="botao-menu"> <img src="Imagens/menuV.png"> </span>-->
+			</div>
+
+			<div class="">
+				
+			</div>
+			  
+			<div class="col-sm-8 d-flex justify-content-start" id="areaMenu">
+			  	<div class="nav-item item-menu">
+				    <a class="nav-link link" href="index.php">Home</a>
+				</div>
+				<div class="nav-item item-menu">
+				    <a class="nav-link link" href="#">Lojas</a>
+				</div>
+				<div class="nav-item item-menu">
+				    <a class="nav-link link" href="#">Sobre</a>
+				</div>
+				<div class="nav-item item-menu">
+				    <a class="nav-link link" href="#">Contato</a>
+				</div>
+			</div>
+
+
+		</nav>
+		
+	</div>
+	<div class="container-fluid NoMercadoEstiloBranco area-menu2">
+		<nav class="navbar navbar-expand-md navbar-dark">
+		  <a href="index.php" class="">
+				<img class="img-fluid logo" alt="Responsive image" src="Imagens/Originais/Vermelho.png">
+		  </a>
+		  <button class="navbar-toggler btn" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+		     <span class="glyphicon glyphicon-menu-hamburger"> <img src="Imagens/menuV.png"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+		    <ul class="navbar-nav">
+		      <li class="nav-item">
+		        <a class="nav-link link" href="#">Home</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link link" href="#">Lojas</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link link" href="#">Sobre</a>
+		      </li> 
+		      <li class="nav-item">
+		        <a class="nav-link link" href="#">Contato</a>
+		      </li>    
+		    </ul>
+		  </div>  
+		</nav>
+	</div>
+
+	Ola
+
+</body>
+</html>
