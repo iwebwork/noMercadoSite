a0971a54f20e1fd42ea7638b146c3708eb6747a8
iwebwork <iwebwork@gmail.com>
2018-12-20 18:17:46 -0200

    Area 1 criada, com slogan e dois slides

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Site noMercado
+- 15/12/20-18: Menu do site criado.
+- 20/12/2018: Area 1 criada no site, com o slogan e dois slides.
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <!--  CSS pessoais -->
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="css/_nav.css">
-
+<link rel="stylesheet" type="text/css" href="css/bodyInicial.css">
 <!--  JS pessoais -->
 <script type="text/javascript" src="js/_nav.js"></script>
 <!-- Latest compiled and minified CSS -->
@@ -57,8 +57,33 @@
 			</div>
 		</nav>
 	</div>-->
-
 	<div class="container-fluid NoMercadoEstiloBranco area-menu">
+		<div class="row">
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			
+			
+		</div>
+	</div>
+	<div class="container-fluid NoMercadoEstiloBranco area-menu">
+		
 		<nav class="navbar navbar-expand-md navbar-dark">
 		  	<div class="col-sm-4 area-logo d-flex justify-content-center">
 				<a href="index.php" class="">
@@ -86,11 +111,26 @@
 				</div>
 			</div>
 
-
 		</nav>
 		
 	</div>
 	<div class="container-fluid NoMercadoEstiloBranco area-menu2">
+		<div class="row">
+			
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			
+			
+		</div>
 		<nav class="navbar navbar-expand-md navbar-dark">
 		  <a href="index.php" class="">
 				<img class="img-fluid logo" alt="Responsive image" src="Imagens/Originais/Vermelho.png">
@@ -117,7 +157,86 @@
 		</nav>
 	</div>
 
-	Ola
+	<!-- Inicio do corpo do site -->
+	<div class="container-fluid area1-container">
 
+		<div class="row justify-content-around">
+			
+			<div class="col-sm-3 NoMercadoEstilo">
+				
+				
+				<div class="d-flex align-items-center">
+					<div class="area1-itens">
+						<h6 class="text-center">"Se o Tom usasse o No Mercado, não precisaria correr atras do Jerry."</h6>
+			
+					
+						<img src="Imagens/Originais/Branco.png" class="img-fluid rounded-0"  alt="Responsive image">
+					</div>
+				</div>
+
+			</div>
+			
+			<div class="col-sm-3 NoMercadoEstiloBranco">
+				
+					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner">
+						    <div class="carousel-item active slides1-corpo">
+						    	<div class="text-center">
+						     		<img src="Imagens/time-passing.png" class="img-fluid"  alt="Responsive image">
+						     	</div>
+						     	<p class="text-center">Economize tempo:</p>
+						     	<p>Receba suas compras em casa em menos de 1 hora.</p> 
+						     	
+						    </div>
+						    <div class="carousel-item slides1-corpo">
+						      	<div class="text-center">
+						     		<img src="Imagens/gift.png" class="img-fluid"  alt="Responsive image">
+						     	</div>
+						     	<p class="text-center">Em mãos experientes:</p>
+						     	<p>Um shopper seleciona seus produtos com amor.</p>
+						    </div>
+						    <div class="carousel-item slides1-corpo">
+						      	<div class="text-center">
+						     		<img src="Imagens/medal (1).png" class="img-fluid"  alt="Responsive image">
+						     	</div>
+						     	<p class="text-center">Comprovada:</p>
+						     	<p>Se não gostar do estado de algum produto, não pagará por ele!</p>
+
+						    </div>
+						</div>
+					</div>
+				
+			</div>
+			
+			<div class="col-sm-3 NoMercadoEstiloBranco">
+				<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner">
+						    <div class="carousel-item active slides1-corpo">
+						    	<div class="text-center">
+						     		<img src="Imagens/painting-palette.png" class="img-fluid"  alt="Responsive image">
+						     	</div>
+						     	<p class="text-center">Qualidade:</p>
+						     	<p>A simplicidade é a máxima sofisticação. Um app simples, fácil e leve de utilizar.</p> 
+						     	
+						    </div>
+						    <div class="carousel-item slides1-corpo">
+						      	<div class="text-center">
+						     		<img src="Imagens/house.png" class="img-fluid"  alt="Responsive image">
+						     	</div>
+						     	<p class="text-center">Comodidade:</p>
+						     	<p>Receba suas compras em sua casa com total comodidade e segurança..</p>
+						    </div>
+						    
+						</div>
+				</div>
+				
+			</div>
+			
+		</div>
+
+	</div>
+
+	
+	<!-- Fim do corpo do site -->
 </body>
 </html>
