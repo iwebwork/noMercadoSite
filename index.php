5573d5fe2b7ed07f7fb7c38b6d2c79b75824364c
iwebwork <iwebwork@gmail.com>
2018-12-20 21:38:01 -0200

    Pagina inicial finalizada

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 <link rel="stylesheet" type="text/css" href="css/bodyInicial.css">
 <!--  JS pessoais -->
 <script type="text/javascript" src="js/_nav.js"></script>
+<script type="text/javascript" src="js/_date.js"></script>
+
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
@@ -25,7 +27,7 @@
 
 
 </head>
-<body class="body">
+<body class="body" onload="date()">
 	<!--
 	<div class="container-fluid NoMercadoEstilo area-menu">
 		<nav class="">
@@ -162,23 +164,8 @@
 
 		<div class="row justify-content-around">
 			
-			<div class="col-sm-3 NoMercadoEstilo">
-				
-				
-				<div class="d-flex align-items-center">
-					<div class="area1-itens">
-						<h6 class="text-center">"Se o Tom usasse o No Mercado, não precisaria correr atras do Jerry."</h6>
-			
-					
-						<img src="Imagens/Originais/Branco.png" class="img-fluid rounded-0"  alt="Responsive image">
-					</div>
-				</div>
-
-			</div>
-			
 			<div class="col-sm-3 NoMercadoEstiloBranco">
-				
-					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+				<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
 						<div class="carousel-inner">
 						    <div class="carousel-item active slides1-corpo">
 						    	<div class="text-center">
@@ -205,6 +192,20 @@
 						    </div>
 						</div>
 					</div>
+				
+
+			</div>
+			
+			<div class="col-sm-3 NoMercadoEstilo">
+					<div class="d-flex align-items-center">
+						<div class="area1-itens">
+							<h6 class="text-center">"Se o Tom usasse o No Mercado, não precisaria correr atras do Jerry."</h6>
+				
+						
+							<img src="Imagens/Originais/Branco.png" class="img-fluid rounded-0"  alt="Responsive image">
+						</div>
+					</div>
+				
 				
 			</div>
 			
@@ -236,7 +237,95 @@
 
 	</div>
 
-	
+	<div class="container-fluid area1-container NoMercadoEstiloBranco">
+
+		<div class="row">
+			<div class="col-sm-2 NoMercadoEstilo">
+				
+			</div>
+			<div class="col-sm-8 text-center">
+				<h4 class="text-xl text-center">
+					Nosso Objetivo
+				</h4>
+				<article>
+					<p class="text-xl-left">
+						Para atender as demandas de uma maneira eficaz, em um ambiente caracterizado pela velocidade das mudanças, torna-se indispensável um modelo de gerenciamento baseado no foco em prioridades e objetivos.
+						Analisando o principal modelo de negócio para o consumidor, observamos que o mesmo fica limitado a respeito das compras que são realizadas no que se diz a respeito dos Supermercados. Tendo em vista que a metodologia de compra mais usual do consumidor é presencial, o mesmo pode ter várias interferências a respeito de uma compra. Consequentemente essa principal metodologia acaba inviabilizando uma maior movimentação financeira e deixa o consumidor com uma menor abrangência no que rege às compras realizadas.
+						Seguindo as informações já supracitadas, adaptamos para o consumidor um modelo de negócio que se adapte totalmente a sua necessidade. Permitindo com que o mesmo tenha uma maior flexibilidade e comodidade ao realizar um compra.
+					</p>
+				</article>
+			</div>
+			
+			<div class="col-sm-2 NoMercadoEstilo">
+				
+			</div>
+		</div>
+	</div>
+	<!-- Inicio do rodapé do site -->
+	<div class="container-fluid area1-container NoMercadoEstiloBranco">
+		<div class="row">
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+			<div class="col"></div>
+			<div class="col NoMercadoEstilo"></div>
+
+		</div>
+
+		<div class="row d-flex justify-content-center">
+
+			<div class="col-sm-4" >
+				<h6 class="text-center">Integrantes</h6>
+				<div class="list-integrantes">
+					<p class="">Lucas de Assis Siqueira: Desenvolvedor Aplicativo</p>
+					<p class="">Bruno Mansour Salim Mourawad E Silva: Marketing</p>
+					<p class="">Guilherme Vinícius Torres da Silva: Administração</p>
+					<p class="">Igor Siqueira da Silva: Desenvolvedor Web</p>
+				</div>
+			</div>
+			<div class="col-sm-2" ></div>
+			<div class="col-sm-2" >
+				<div >
+					<h6>Baixe o Aplicativo</h6>
+					<div class="area-app">
+						<a href="https://play.google.com/store/apps/details?id=com.oficial.nomercado&hl=pt_BR" target="_blank">
+							<img class="img-fluid" src="Imagens/google play.png" alt="Chania">
+						</a>
+					</div>
+				</div>
+
+				<div>
+					<h6>Siga-nos</h6>
+					<span class="area-rede">
+						<a href="http://twitter.com/nomercadobr" target="_blank">
+							<img class="img-fluid" src="Imagens/twitter.png" alt="Chania">
+						</a>
+					</span>
+					<span class="area-rede">
+						<a href="http://instagram.com/nomercadobr" target="_blank">
+							<img class="img-fluid" src="Imagens/instagram.png" alt="Chania">
+						</a>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid NoMercadoEstiloBranco">
+		
+		<div class="row">
+			<div class="col-sm d-flex justify-content-center">
+				<h6>No Mercado - Todos os direitos reservados - <data id="data"></data> </h6>
+			</div>
+		</div>
+	</div>
+	<!-- Fim do rodapé do site -->
 	<!-- Fim do corpo do site -->
 </body>
 </html>
--- js/_date.js
+++ js/_date.js
@@ -1,6 +1,5 @@
 //Este arquivo e usado para sempre deixar o texto com o ano no site atualizado
 function date(){
-    
    year = new Date();
    ano = year.getFullYear();
    document.getElementById("data").innerHTML = ano;
